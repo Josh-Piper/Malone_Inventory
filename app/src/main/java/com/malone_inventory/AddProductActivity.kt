936d4832099e7262cf6f57cf526a7acb98f1e740
PandaPlaysAll <josh.piper@live.com.au>
2020-06-02 13:52:52 +1000

    P 3

---
+++ app/src/main/java/com/malone_inventory/AddProductActivity.kt
@@ -0,0 +1,14 @@
+package com.malone_inventory
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class AddProductActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.add_product)
+    }
+
+}
